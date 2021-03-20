e99faccb1de1bd3afcfd4046381e58a8183e9252
janiszek <kjaniszewski@interia.pl>
2021-03-20 12:39:37 +0100

    add logic do get data from endpoint and load into DF

---
+++ firstscript.R
@@ -0,0 +1,13 @@
+install.packages("httr")
+install.packages("jsonlite")
+library(httr)
+library(jsonlite)
+#require()
+
+print("hello")
+endpoint <- "https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
+getWeather<- GET(endpoint)
+#getWeather$content
+weatherText <- content(getWeather,"text")
+weatherJson <- fromJSON(weatherText,flatten = TRUE)
+
